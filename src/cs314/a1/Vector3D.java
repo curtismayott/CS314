c09c2d79cbfab7fcc1ec857d1fa06c0da5255cf9
darkbobo <darkbobo@localhost.localdomain>
2015-01-21 20:07:15 -0700

    files

---
+++ src/cs314/a1/Vector3D.java
@@ -0,0 +1,13 @@
+package cs314.a1;
+
+public class Vector3D {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("woooo\n");
+	}
+
+}
