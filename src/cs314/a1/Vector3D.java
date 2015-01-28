ce905f3dea45de3447787622f9491f8c28e5dd47
darkbobo <darkbobo@localhost.localdomain>
2015-01-27 17:12:57 -0700

    git should now be current to current work.

--- src/cs314/a1/Vector3D.java
+++ src/cs314/a1/Vector3D.java
@@ -1,13 +1,33 @@
 package cs314.a1;
 
 public class Vector3D {
-
-	/**
-	 * @param args
-	 */
+	double x;
+	double y;
+	double z;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("woooo\n");
 	}
-
+	public Vector3D(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	public void scale(double f){
+		this.x *= f;
+		this.y *= f;
+		this.z *= f;
+	}
+	public Vector3D add(Vector3D v){
+		return new Vector3D(this.x + v.x, this.y + v.y, this.z + v.z);
+	}
+	
+	
+	// override equalto for testing
+	public boolean equals(Vector3D v){
+		return this.x == v.x && this.y == v.y && this.z == v.z;
+	}
+	public String toString(){
+		return "{" + this.x + " " + this.y + " " + this.z + "}";
+	}
 }
